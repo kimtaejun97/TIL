0d63465a5f72ef2b71484e6cad04621c53b4b311
kimtaejun <kim_taejun@naver.com>
2021-06-27 14:10:38 +0900

    의존성 관리 이해

---
+++ Spring/SpringBoot/spring-boot-principle/src/main/java/com/bigave/springbootprinciple/SpringBootPrincipleApplication.java
@@ -0,0 +1,13 @@
+package com.bigave.springbootprinciple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPrincipleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootPrincipleApplication.class, args);
+    }
+
+}
---
+++ Spring/SpringBoot/spring-boot-principle/src/test/java/com/bigave/springbootprinciple/SpringBootPrincipleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bigave.springbootprinciple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootPrincipleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
