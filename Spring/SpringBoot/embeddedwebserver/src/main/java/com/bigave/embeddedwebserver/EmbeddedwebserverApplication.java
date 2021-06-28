83e25d747516f8fbf23041c7da18d84121f2285f
kimtaejun <kim_taejun@naver.com>
2021-06-28 10:30:21 +0900

    내장 웹서버

---
+++ Spring/SpringBoot/embeddedwebserver/src/main/java/com/bigave/embeddedwebserver/EmbeddedwebserverApplication.java
@@ -0,0 +1,13 @@
+package com.bigave.embeddedwebserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmbeddedwebserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmbeddedwebserverApplication.class, args);
+    }
+
+}
---
+++ Spring/SpringBoot/embeddedwebserver/src/test/java/com/bigave/embeddedwebserver/EmbeddedwebserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bigave.embeddedwebserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmbeddedwebserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
