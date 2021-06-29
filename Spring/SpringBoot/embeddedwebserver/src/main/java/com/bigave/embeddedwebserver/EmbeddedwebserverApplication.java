f08dc7345d61086e87612b0cbb7c93123a75887c
kimtaejun <kim_taejun@naver.com>
2021-06-29 21:15:14 +0900

    tomcat

--- Spring/SpringBoot/embeddedwebserver/src/main/java/com/bigave/embeddedwebserver/EmbeddedwebserverApplication.java
+++ Spring/SpringBoot/embeddedwebserver/src/main/java/com/bigave/embeddedwebserver/EmbeddedwebserverApplication.java
@@ -1,13 +1,23 @@
 package com.bigave.embeddedwebserver;
 
+import org.apache.catalina.LifecycleException;
+import org.apache.catalina.startup.Tomcat;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
 
 @SpringBootApplication
 public class EmbeddedwebserverApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(EmbeddedwebserverApplication.class, args);
+    public static void main(String[] args) throws LifecycleException {
+//        SpringApplication.run(EmbeddedwebserverApplication.class, args);
+        Tomcat tomcat = new Tomcat();
+        tomcat.setPort(8080);
+
+        tomcat.addContext("/", "/");
+
+        tomcat.start();
     }
 
 }
