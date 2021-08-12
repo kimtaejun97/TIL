aaa8746d8adb2d285364d42d83b0f32143eb5f97
kimtaejun <kim_taejun@naver.com>
2021-08-12 21:19:48 +0900

    멀티 화상통화를 위한WebRTC 공부

---
+++ Spring/webrtc/src/test/java/com/webrtc/WebrtcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webrtc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebrtcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
