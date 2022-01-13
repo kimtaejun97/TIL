8c4e2c704f9d0775acf70dc655cb5c562027fe12
kimtaejun97 <kim_taejun@naver.com>
2022-01-13 21:19:08 +0900

    docs: javascript 기초지식
    자료형, 함수, 변수, DOM API

--- Frontend/html:css:js/html:css:js/js/main.js
+++ Frontend/html:css:js/html:css:js/js/main.js
@@ -1,1 +1,33 @@
-console.log('Hello ~~')
+
+
+// classList
+let boxEl = document.querySelector('.box');
+
+boxEl.classList.add('active'); // class 추가.
+  
+let isContains = boxEl.classList.contains('active'); // 포함 여부.
+console.log(isContains);
+
+boxEl.classList.remove('active'); // class 제거.
+
+isContains = boxEl.classList.contains('active'); // 포함 여부.
+console.log(isContains);
+
+
+
+// forEach
+let boxEls = document.querySelectorAll('.box');
+
+boxEls.forEach(function(boxEl, index){
+  console.log(index);
+  console.log(boxEls);
+
+  boxEl.classList.add(`order-${index+1}`)
+});
+
+// textContent
+  // getter
+let content = boxEl.textContent;
+
+  //setter
+boxEl.textContent = 'box!!';
