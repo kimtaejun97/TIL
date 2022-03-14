dfb0b3f8e4e343930f42bd1977c86a37fd1d49d5
kimtaejun97 <kim_taejun@naver.com>
2022-03-15 01:23:48 +0900

    SpringCloud: EurekaServer Setup

---
+++ Spring/SpringCloud/discoveryservice/src/test/java/com/study/discoveryservice/DiscoveryserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.discoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
